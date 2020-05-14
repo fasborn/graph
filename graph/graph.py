d786b11924be935a3cec6bf39c09ebe757ebc516
fasborn <fasborn@gmail.com>
2020-05-14 19:33:58 +0300

    v2

--- graph/graph.py
+++ graph/graph.py
@@ -6,6 +6,8 @@ from matplotlib import animation
 import collections
 
 class SquareGrid:
+    visited_cells = {}
+    
     def __init__(self, bounds, step):
         self.xmin, self.ymin, self.xmax, self.ymax = bounds
         self.step = step
@@ -15,11 +17,21 @@ class SquareGrid:
 #         else:
 #             raise ValueError("Point given is not valid!")
         self.stations = {}
-        self.visited_cells = {}
         
     def in_bounds(self, id):
         (x, y) = id
         return self.xmin <= x < self.xmax and self.ymin <= y < self.ymax
+    
+    def is_valid_point(self, id):
+        (x, y) = id
+        return (not bool(((x-self.step/2)%self.step) and bool((y-self.step/2)%self.step))) and self.in_bounds(id)
+    
+    def is_valid_point_err(self, id):
+        (x, y) = id
+        if not(bool((x-self.step/2)%self.step)) and not(bool((y-self.step/2)%self.step)) and self.in_bounds(id):
+            return id
+        else:
+            raise ValueError("Point given is not valid!")
     
     def neighbors(self, id):
         if self.is_valid_point(id):
@@ -31,21 +43,14 @@ class SquareGrid:
         else:
             raise ValueError("Point given is not valid!")
     
-    def is_valid_point(self, id):
-        (x, y) = id
-        return (not bool(((x-self.step/2)%self.step) and bool((y-self.step/2)%self.step))) and self.in_bounds(id)
-    
-    def is_valid_point_err(self, id):
-        (x, y) = id
-        if (not bool(((x-self.step/2)%self.step) and bool((y-self.step/2)%self.step))) and self.in_bounds(id):
-            return id
-        else:
-            raise ValueError("Point given is not valid!")
-    
     def add_station(self, id):
         # Единичная проверка на валидность узла в рамках сетки
-        self.stations[is_valid_point_err(id)] = Station(id, self.bounds, self.step)
-        self.visited_cells{id}
+        if id not in self.stations:
+            self.stations[self.is_valid_point_err(id)] = Station(id, self.bounds, self.step)
+            self.visited_cells[id] = id
+            return self.stations[id]
+        else:
+            raise ValueError("Such station already exists!")
 
 class Queue:
     def __init__(self):
@@ -64,21 +69,22 @@ class Station(SquareGrid):
     def __init__(self, id, bounds, step):
         self.xmin, self.ymin, self.xmax, self.ymax = bounds
         self.step = step
-        self.queue = Queue()
-        self.queue.put(id)
         if self.is_valid_point(id):
             self.coordinates = id
         else:
             raise ValueError("Point given is not valid!")
+        self.queue = Queue()
+        self.queue.put(id)
+        self.visited_cells = SquareGrid.visited_cells
             
-    def expand_border(self, dct):
+    def expand_border(self):
         if not self.queue.empty():
             for cell in list(self.queue.elements):
                 current = self.queue.get()
                 for neighbor in self.neighbors(current):
-                    if neighbor not in dct:
-                        self.queue.put(next)
-                        dct[next] = self.coordinates
+                    if neighbor not in self.visited_cells:
+                        self.queue.put(neighbor)
+                        self.visited_cells[neighbor] = self.coordinates
 
         else:
             raise ValueError("Queue is empty!")
