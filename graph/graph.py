9461e19b2eccd11da3487da6cb20dbb9b420ba7b
fasborn <fasborn@gmail.com>
2020-05-15 16:37:27 +0300

    New functionality

--- graph/graph.py
+++ graph/graph.py
@@ -7,19 +7,42 @@ import collections
 
 class SquareGrid:
     visited_cells = {}
+    geo_grid = None
     
-    def __init__(self, bounds, step):
-        self.xmin, self.ymin, self.xmax, self.ymax = bounds
+    def __init__(self, data, step: float):
+        #if isinstance(data, GeoDataFrame):
         self.step = step
-        self.bounds = bounds
+         
+        if len(data)==4:
+            self.xmin, self.ymin, self.xmax, self.ymax = data
+            self.bounds = data
+            
+        elif len(data)==2:
+            self.number_of_rows, self.number_of_columns = data
+            self.xmin, self.ymin = 0, 0
+            self.xmax, self.ymax = number_of_rows*step, number_of_columns*step
+            self.bounds = [self.xmin, self.ymin, self.xmax, self.ymax]
+            
         self.stations = {}
+        geo_grid = self.grid()
+    
+#    @attribute
+#    self.geo_grid = selg.grid()
+    
+    def create_array(self):
+        self.columns = list(np.arange(int(np.floor(self.xmin)), int(np.ceil(self.xmax)), self.step))
+        self.rows = list(np.arange(int(np.floor(self.ymin)), int(np.ceil(self.ymax)), self.step))
+        self.rows.reverse()
+        """Method to be implemented"""
+        raise NotImplementedError
+        
         
     def grid(self):
         try:
             return self.geo_grid
         except:
             self.columns = list(np.arange(int(np.floor(self.xmin)), int(np.ceil(self.xmax)), self.step))
-            self.rows = list(np.arange(int(np.floor(self.ymin)), int(np.ceil(self.ymax)), self.step))
+            self.rows = list(np.arange(int(np.floor(self.ymin)), int(np.ceil(self.ymax)), self.step)+self.step)
             self.rows.reverse()
 
             self.geo_grid = gpd.GeoDataFrame(columns = ['geometry'])
@@ -30,7 +53,9 @@ class SquareGrid:
             self.geo_grid['centroid'] = self.geo_grid.geometry.centroid
             self.geo_grid['num'] = self.geo_grid.index + 1
             self.geo_grid['coords'] = self.geo_grid['geometry'].apply(lambda x: x.representative_point().coords[:])
-            self.geo_grid['coords'] = [coords[0] for coords in self.geo_grid['coords']]    
+            self.geo_grid['coords'] = [coords[0] for coords in self.geo_grid['coords']]
+            
+            return self.geo_grid
 
     def in_bounds(self, id):
         (x, y) = id
@@ -48,7 +73,7 @@ class SquareGrid:
             raise ValueError("Point given is not valid!")
     
     def neighbors(self, id):
-        if self.is_valid_point(id):
+        if self.is_valid_point_err(id):
             (x, y) = id
             results = [(x+self.step, y), (x, y-self.step), (x-self.step, y), (x, y+self.step)]
             if (x + y) % 2 == 0: results.reverse() # ради эстетики
@@ -57,30 +82,65 @@ class SquareGrid:
         else:
             raise ValueError("Point given is not valid!")
     
-    def add_station(self, id):
+    def add_station(self,
+                    x = None,
+                    y = None,
+                    id = None):
         # Единичная проверка на валидность узла в рамках сетки
-        if id not in self.stations:
-            self.stations[self.is_valid_point_err(id)] = Station(id, self.bounds, self.step)
-            self.visited_cells[id] = id
-            return self.stations[id]
-        else:
-            raise ValueError("Such station already exists!")
+        if id != None:
+            if id not in self.stations:
+                self.stations[self.is_valid_point_err(id)] = Station(id, self.bounds, self.step)
+                self.visited_cells[id] = id
+                return self.stations[id]
+            else:
+                raise ValueError("Such station already exists!")
+        elif x != None and y != None:
+            id = (x, y)
+            if id not in self.stations:
+                self.stations[self.is_valid_point_err(id)] = Station(id, self.bounds, self.step)
+                self.visited_cells[id] = id
+                return self.stations[id]
+            else:
+                raise ValueError("Such station already exists!")
             
+                    
     def expand_borders(self):
         """Expand borders of each station"""
         raise NotImplementedError
         
-    def plot(self):
+    def plot(self, axis):
         """Plot each artist"""
         raise NotImplementedError
         
+        self.grid()
+        
+        frontier = []
+        
+        for station in self.stations:
+            for cell in station.elements:
+                frontier.append(cell)
+            
+        known_territory = [cell for cell in self.visited_cells.keys() if p not in frontier]
+
+        # known_territory
+#         self.geo_grid[self.geo_grid.coords.isin(known_territory)].plot(ax = ax, color = 'lightcyan', ec = 'bisque')
+
+#         # Frontier cells
+#         self.geo_grid[self.geo_grid.coords.isin(self.queue.elements)].plot(ax = ax, color = 'deepskyblue', ec = 'bisque')
+
+#         # Other cells
+#         self.geo_grid[~self.geo_grid.coords.isin(list(self.visited_cells.keys()))].plot(ax = ax, color = 'cyan', ec = 'bisque')
+
+        
     def set_grid(self):
-        """Attribute to implement"""
+        """Attribute to be implemented"""
         raise NotImplementedError
 
     def set_rows_columns(self):
         """Attributes to be implemented"""
         raise NotImplementedError
+
+
 
 
 class Queue:
@@ -97,6 +157,7 @@ class Queue:
         return self.elements.popleft()
 		
 class Station(SquareGrid):
+
     def __init__(self, id, bounds, step):
         self.xmin, self.ymin, self.xmax, self.ymax = bounds
         self.step = step
@@ -107,6 +168,7 @@ class Station(SquareGrid):
         self.queue = Queue()
         self.queue.put(id)
         self.visited_cells = SquareGrid.visited_cells
+        self.geo_grid = SquareGrid.geo_grid
             
     def expand_border(self):
         if not self.queue.empty():
@@ -119,3 +181,16 @@ class Station(SquareGrid):
 
         else:
             raise ValueError("Queue is empty!")
+            
+    def plot_step(self):
+        visited = [p for p in self.visited_cells.keys() if p not in self.queue.elements]
+        ax = plt.gca()
+
+        # Visited cells
+        self.geo_grid[self.geo_grid.coords.isin(visited)].plot(ax = ax, color = 'lightcyan', ec = 'bisque')
+
+        # Frontier cells
+        self.geo_grid[self.geo_grid.coords.isin(self.queue.elements)].plot(ax = ax, color = 'deepskyblue', ec = 'bisque')
+
+        # Other cells
+        self.geo_grid[~self.geo_grid.coords.isin(list(self.visited_cells.keys()))].plot(ax = ax, color = 'cyan', ec = 'bisque')
